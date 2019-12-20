b325d1ddb384992472562195c8632d2333668265
Qui <lquiitfuture@gmail.com>
2019-12-20 10:00:12 +0700

    Updte

--- Program.cs
+++ Program.cs
@@ -33,33 +33,55 @@ namespace Generalized_Async_Return_Types
          }
          */
         //example 2
+        /*  public static void Main()
+          {
+              Console.WriteLine(ShowTodaysInfo().Result);
+              Console.WriteLine("Press any key to exist.");
+              Console.ReadKey();
+          }
+          private static async Task<string> ShowTodaysInfo()
+          {
+              var infoTask = GetLeisureHours();
+              // You can do other work that does not rely on integerTask before awaiting.
+              string ret = $"Today is {DateTime.Today:D}\n" +
+                           "Today's hours of leisure: " +
+                           $"{await infoTask}";
+              return ret;
+          }
+          static async Task<int> GetLeisureHours()
+          {
+              // Task.FromResult is a placeholder for actual work that returns a string.  
+              var today = await Task.FromResult<string>(DateTime.Now.DayOfWeek.ToString());
+              // The method then can process the result in some way.  
+              int leisureHours;
+              if (today.First() == 'S')
+                  leisureHours = 16;
+              else
+                  leisureHours = 5;
+              return leisureHours;
+          }
+          */
+        //The async method returning Task
         public static void Main()
         {
-            Console.WriteLine(ShowTodaysInfo().Result);
+            DisplayCurrentInfo().Wait();
             Console.WriteLine("Press any key to exist.");
             Console.ReadKey();
         }
-        private static async Task<string> ShowTodaysInfo()
+        static async Task DisplayCurrentInfo()
         {
-            var infoTask = GetLeisureHours();
-            // You can do other work that does not rely on integerTask before awaiting.
-            string ret = $"Today is {DateTime.Today:D}\n" +
-                         "Today's hours of leisure: " +
-                         $"{await infoTask}";
-            return ret;
+            await WaitAndApologize();
+            Console.WriteLine($"Today is {DateTime.Now:D}");
+            Console.WriteLine($"The current time is {DateTime.Now.TimeOfDay:t}");
+            Console.WriteLine("The current temperature is 76 degrees.");
         }
-        static async Task<int> GetLeisureHours()
+        static async Task WaitAndApologize()
         {
-            // Task.FromResult is a placeholder for actual work that returns a string.  
-            var today = await Task.FromResult<string>(DateTime.Now.DayOfWeek.ToString());
-            // The method then can process the result in some way.  
-            int leisureHours;
-            if (today.First() == 'S')
-                leisureHours = 16;
-            else
-                leisureHours = 5;
-            return leisureHours;
+            // Task.Delay is a placeholder for actual work.  
+            await Task.Delay(2000);
+            // Task.Delay delays the following line by two seconds.  
+            Console.WriteLine("\nSorry for the delay. . . .\n");
         }
-
     }
+
 }
